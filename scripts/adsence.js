250d22625fc306e83fb03f468bec3d15e1297019
Aye10032 <aye10032@gmail.com>
2022-10-23 17:53:08 +0800

    update

--- scripts/adsence.js
+++
@@ -1,3 +0,0 @@
-hexo.extend.filter.register('theme_inject', function(injects) {
-    injects.postLeft.raw('default', '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2406358340653849" data-ad-slot="9875755322" data-ad-format="auto" data-full-width-responsive="true"></ins> <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>');
-  });
